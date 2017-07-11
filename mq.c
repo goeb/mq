a84f6576cf0d03b7ecbf679a5e43a360bc9934c3
Frederic Hoerni <fhoerni@free.fr>
2017-07-11 22:52:18 +0200

    v1.0

--- mq.c
+++ mq.c
@@ -3,18 +3,35 @@
 #include <mqueue.h>
 #include <errno.h>
 #include <string.h>
+#include <stdint.h>
 
 #define PROG_NAME "mq"
+const char *argp_program_version = PROG_NAME " 1.0";
 
-static char doc[] = "A command line tool to use Posix Message Queues from the shell"
-					"\vCommands:\n"
-					"  create    Create a POSIX message queue\n"
-					"  info      Print information about an existing message queue\n"
-					"  unlink    Delete a message queue\n"
-					"  send      Send a message to a message queue\n"
-					"  recv      Receive and print a message from a message queue\n"
+static char doc[] = 
+	"A command line tool to use Posix Message Queues from the shell"
+	"\vCommands:\n"
+	"  create    Create a POSIX message queue\n"
+	"  info      Print information about an existing message queue\n"
+	"  unlink    Delete a message queue\n"
+	"  send      Send a message to a message queue\n"
+	"  recv      Receive and print a message from a message queue\n"
+	"\n"
+	"Examples:\n"
+	"  mq create /myqueue\n"
+	"  mq send /myqueue \"hello\" -n\n"
+	"  mq info /myqueue\n"
+	"  mq recv /myqueue\n"
+	"  mq unlink /myqueue\n"
 					 ;
-static char args_doc[] = "COMMAND [ MESSAGE ]";
+
+static char args_doc[] =
+	"create QNAME\n"
+	"info QNAME\n"
+	"unlink QNAME\n"
+	"send QNAME MESSAGE\n"
+	"recv QNAME"
+	;
 
 void usage(const struct argp *argp)
 {
@@ -23,6 +40,9 @@ void usage(const struct argp *argp)
 
 static struct argp_option options[] = {
 	{ "verbose", 'v', 0, 0, "Produce verbose output" },
+	{ "non-blocking", 'n', 0, 0, "Do not block (send, recv)" },
+	{ "msgsize", 's', "SIZE", 0, "Message size in bytes (create)" },
+	{ "maxmsg", 'm', "NUMBER", 0, "Maximum number of messages in queue (create)" },
 	{ 0 }
 };
 
@@ -35,7 +55,6 @@ struct arguments
 	/* for command 'create' */
 	int maxmsg; /* max number of message */
 	int msgsize; /* size of a message */
-	int force; /* do not return an error if queue already exists */
 
 	/* for command 'recv' */
 	int blocking;
@@ -46,8 +65,6 @@ struct arguments
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state)
 {
-	/* Get the input argument from argp_parse, which we
-	 * know is a pointer to our arguments structure. */
 	struct arguments *args = state->input;
 
 	switch (key) {
@@ -55,30 +72,36 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
 	  args->verbose = 1;
 	  break;
 
+	case 'n':
+	  args->blocking = 0;
+	  break;
+
+	case 's':
+	  args->msgsize = atoi(arg);
+	  break;
+
+	case 'm':
+	  args->maxmsg = atoi(arg);
+	  break;
+
 	case ARGP_KEY_NO_ARGS:
 	  argp_usage(state);
 
 	case ARGP_KEY_ARG:
-		/* Here we know that state->arg_num == 0, since we
-		   force argument parsing to end before any more arguments can
-		   get here. */
-		args->command = arg;
-
-		/* Now we consume all the rest of the arguments.
-		   state->next is the index in state->argv of the
-		   next argument to be parsed, which is the first string
-		   we’re interested in, so we can just use
-		   &state->argv[state->next] as the value for
-		   arguments->strings.
-
-		   In addition, by setting state->next to the end
-		   of the arguments, we can force argp to stop parsing here and
-		   return. */
-		args->qname = state->argv[state->next];
-		args->message = state->argv[state->next+1];
-		state->next = state->argc;
+		if (!args->command) args->command = arg;
+		else if (!args->qname) args->qname = arg;
+		else if (0 == strcmp(args->command, "send") && !args->message) args->message = arg;
+		else {
+			/* too many arguments */
+			argp_usage(state);
+		}
 
 		break;
+
+	case ARGP_KEY_END:
+		if (!args->command) argp_usage(state);
+		if (!args->qname) argp_usage(state);
+		if (0 == strcmp(args->command, "send") && !args->message) argp_usage(state);
 
 	default:
 		return ARGP_ERR_UNKNOWN;
@@ -94,34 +117,114 @@ static int mqu_create(const struct arguments *args)
 	attr.mq_msgsize = args->msgsize;
 	attr.mq_curmsgs = 0;
 
-	mqd_t queue = mq_open(args->qname, O_CREAT|O_RDWR|O_EXCL, 0644, &attr);
+	mode_t mode = 0644;
+
+	if (args->verbose) fprintf(stderr, "Opening mq %s (O_CREAT, O_RDWR, O_EXCL, %o)\n", args->qname, mode);
+
+	mqd_t queue = mq_open(args->qname, O_CREAT|O_RDWR|O_EXCL, mode, &attr);
 
 	if (-1 == queue) {
 		printf("mq_open error: %s\n", strerror(errno));
-		exit(1);
+		return 1;
 	}
+	return 0;
 }
 
 static int mqu_info(const struct arguments *args)
 {
+	struct mq_attr attr;
+	int ret;
+
+	if (args->verbose) fprintf(stderr, "Opening mq %s (O_RDONLY)\n", args->qname);
+	mqd_t queue = mq_open(args->qname, O_RDONLY);
+	if (-1 == queue) {
+		printf("mq_open error: %s\n", strerror(errno));
+		return 1;
+	}
+
+	ret = mq_getattr(queue, &attr);
+	if (0 != ret) {
+		printf("mq_getattr error: %s\n", strerror(errno));
+		ret = 1;
+	} else {
+		printf("%s: maxmsg=%ld, msgsize=%ld, curmsgs=%ld\n",
+				args->qname, attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
+	}
+
+	mq_close(queue);
+	return ret;
 }
 
 static int mqu_unlink(const struct arguments *args)
 {
+	if (args->verbose) fprintf(stderr, "Deleting mq %s\n", args->qname);
 	int ret = mq_unlink(args->qname);
 
 	if (0 != ret) {
 		printf("mq_unlink error: %s\n", strerror(errno));
-		exit(1);
+		return 1;
 	}
+	return 0;
 }
 
 static int mqu_send(const struct arguments *args)
 {
+	int oflag = O_WRONLY;
+	if (!args->blocking) oflag |= O_NONBLOCK;
+
+	if (args->verbose) fprintf(stderr, "Opening mq %s (O_WRONLY, %s)\n", args->qname, (oflag & O_NONBLOCK)?"O_NONBLOCK":"");
+	mqd_t queue = mq_open(args->qname, oflag);
+	if (-1 == queue) {
+		printf("mq_open error: %s\n", strerror(errno));
+		return 1;
+	}
+
+	if (args->verbose) fprintf(stderr, "Sending to mq %s: %s\n", args->qname, args->message);
+	int ret = mq_send(queue, args->message, strlen(args->message)+1, 1); // keep the null terminating char
+	if (0 != ret) {
+		printf("mq_send error: %s\n", strerror(errno));
+		ret = 1;
+	}
+
+	mq_close(queue);
+	return ret;
 }
 
 static int mqu_recv(const struct arguments *args)
 {
+	mqd_t queue;
+	int ret;
+	uint8_t *buffer;
+	struct mq_attr attr;
+	int oflag = O_RDONLY;
+	if (!args->blocking) oflag |= O_NONBLOCK;
+
+	if (args->verbose) fprintf(stderr, "Opening mq %s (O_RDONLY, %s)\n", args->qname, (oflag & O_NONBLOCK)?"O_NONBLOCK":"");
+	queue = mq_open(args->qname, oflag);
+	if (-1 == queue) {
+		printf("mq_open error: %s\n", strerror(errno));
+		return 1;
+	}
+
+	// retrieve the message size
+	ret = mq_getattr(queue, &attr);
+	if (0 != ret) {
+		printf("mq_getattr error: %s\n", strerror(errno));
+		mq_close(queue);
+		return 1;
+	}
+
+	buffer = malloc(attr.mq_msgsize);
+
+	ret = mq_receive(queue, buffer, attr.mq_msgsize, NULL);
+	if (ret >= 0) {
+		/* got a message */
+		printf("%s\n", buffer);
+		ret = 0;
+	}
+
+	mq_close(queue);
+	return ret;
 }
 
 int main(int argc, char **argv)
@@ -135,8 +238,7 @@ int main(int argc, char **argv)
 	args.qname = NULL;
 	args.maxmsg = 10;
 	args.msgsize = 1024;
-	args.force = 0;
-	args.blocking = 0;
+	args.blocking = 1;
 	args.message = NULL;
 
 	argp_parse(&argp, argc, argv, 0, 0, &args);
